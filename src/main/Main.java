4e94cc97f64e0b145e290b4c1b6ee633fb92f624
Bjørnar W. Risdalen <bwendelborgrisdalen@gmail.com>
2020-03-06 12:15:03 +0100

    Fixed table. new variables

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,17 +1,27 @@
 package main;
 
+import node.Node;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     int[][] distances;
+    List<Node> inital;
 
     public Main() {
         this.distances = new int[6][6];
+        inital = new ArrayList<>();
+
         distances[0] = new int[]{0, 10, 5, 9999, 3, 12};
         distances[1] = new int[]{-1, 0, 17, 9, 17, 12};
         distances[2] = new int[]{-1, -1, 0, 35, 3, 12};
         distances[3] = new int[]{-1, -1, -1, 0, 999, 12};
         distances[4] = new int[]{-1, -1, -1, -1, 0, 12};
-        distances[5] = new int[]{-1, -1, -1, -1, -1, 0, 12};
+        distances[5] = new int[]{-1, -1, -1, -1, -1, -1, 0};
+
+        
     }
 
     public static void main(String[] args) {
