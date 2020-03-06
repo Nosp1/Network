6ae60636c52f3056e1373ccad13b49578c046d5b
Bjørnar W. Risdalen <bwendelborgrisdalen@gmail.com>
2020-03-06 12:06:37 +0100

    started on Node. Initial array in main.

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,18 @@ package main;
 
 public class Main {
 
+    int[][] distances;
+
+    public Main() {
+        this.distances = new int[6][6];
+        distances[0] = new int[]{0, 10, 5, 9999, 3, 12};
+        distances[1] = new int[]{-1, 0, 17, 9, 17, 12};
+        distances[2] = new int[]{-1, -1, 0, 35, 3, 12};
+        distances[3] = new int[]{-1, -1, -1, 0, 999, 12};
+        distances[4] = new int[]{-1, -1, -1, -1, 0, 12};
+        distances[5] = new int[]{-1, -1, -1, -1, -1, 0, 12};
+    }
+
     public static void main(String[] args) {
 	// write your code here
     }
--- src/node/Node.java
+++ src/node/Node.java
@@ -1,6 +1,18 @@
 package node;
 
+import java.util.List;
+
 public class Node {
 
+    boolean visited;
+    List<Node> adjacent;
 
+    public Node(List<Node> adjacent) {
+        this(false, adjacent);
+    }
+
+    public Node(boolean visited, List<Node> adjacent) {
+        this.visited = visited;
+        this.adjacent = adjacent;
+    }
 }
