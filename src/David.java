cfac54b538b23424485a819404ccdcd81cf2634c
Nosp1 <trymstaur@hotmail.com>
2020-03-23 23:30:39 +0100

    viser david branching

---
+++ src/David.java
@@ -0,0 +1,5 @@
+public class David {
+
+    public David() {
+    }
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -16,22 +16,22 @@ public class Main {
         inital = new ArrayList<>();
         String[] names = {"A", "B", "C", "D", "E", "F"};
 
-        distances[0] = new int[]{ 0, 10,  5, 9999,   3,   12};
-        distances[1] = new int[]{-1,  0, 17,    9,  17,   12};
-        distances[2] = new int[]{-1, -1,  0,   35,   3,   12};
-        distances[3] = new int[]{-1, -1, -1,    0, 999,   12};
-        distances[4] = new int[]{-1, -1, -1,   -1,   0,   12};
-        distances[5] = new int[]{-1, -1, -1,   -1,  -1,    0};
+        distances[0] = new int[]{0, 10, 5, 9999, 3, 12};
+        distances[1] = new int[]{-1, 0, 17, 9, 17, 12};
+        distances[2] = new int[]{-1, -1, 0, 35, 3, 12};
+        distances[3] = new int[]{-1, -1, -1, 0, 999, 12};
+        distances[4] = new int[]{-1, -1, -1, -1, 0, 12};
+        distances[5] = new int[]{-1, -1, -1, -1, -1, 0};
 
-        for(int i = 0; i < distances[0].length; i++) {
+        for (int i = 0; i < distances[0].length; i++) {
             inital.add(new Node(names[i]));
         }
 
         System.out.println("inital.size() = " + inital.size());
 
-        for(int y = 0; y < distances[0].length; y++) { // først  -> x[0] ->  ned y[] -> x[1] -> ned y[]
-            for(int x = 0; x < distances[0].length; x++) {
-                if(distances[x][y] == -1) {
+        for (int y = 0; y < distances[0].length; y++) { // først  -> x[0] ->  ned y[] -> x[1] -> ned y[]
+            for (int x = 0; x < distances[0].length; x++) {
+                if (distances[x][y] == -1) {
                     break;
                 } else {
                     Edge edge = new Edge(inital.get(x), inital.get(y), distances[x][y]);
@@ -41,9 +41,9 @@ public class Main {
             }
         }
 
-        for(int x = distances[0].length - 1; x >= 0; x--) {
-            for(int y = distances[0].length - 1; y >= 0; y--) {
-                if(distances[x][y] == -1) {
+        for (int x = distances[0].length - 1; x >= 0; x--) {
+            for (int y = distances[0].length - 1; y >= 0; y--) {
+                if (distances[x][y] == -1) {
                     break;
                 } else {
                     int distance = distances[x][y];
@@ -58,9 +58,9 @@ public class Main {
             }
         }
 
-        for(Node n : inital) {
+        for (Node n : inital) {
             System.out.print(n.getName() + ": ");
-            for(Edge e : n.getEdges()) {
+            for (Edge e : n.getEdges()) {
                 System.out.println("\t" + e);
             }
             System.out.println();
@@ -68,6 +68,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-	    new Main();
+        new Main();
     }
 }
